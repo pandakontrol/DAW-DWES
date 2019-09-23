bb95bc51bd930a67beec2706e16f74f0db4b796e
pandakontrol <kevinkontrol97@gmail.com>
2019-09-23 19:14:36 +0200

    php

---
+++ date.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+       
+        <h1>
+            <?php
+
+            $horaActual = date ("g:i:s a");
+            // "g, i, s" indican que la salida sea en horas, minutos y segundos, y "a" indica que se muestre am o pm
+           
+            echo "La hora actual es $horaActual";
+           
+            ?>   
+           
+        </h1>
+       
+    </body>
+</html>
